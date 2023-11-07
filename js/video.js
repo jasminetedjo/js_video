3f77c4a4fa164090b8534d4e572366b372f27578
Jasmine Tedjo <113004929+jasminetedjo@users.noreply.github.com>
2023-11-07 15:28:55 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,5 +1,6 @@
 var video = document.querySelector("#player1");
 
+// Page Load
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
 	video.autoplay = false;
@@ -7,9 +8,47 @@ window.addEventListener("load", function() {
 	video.loop = false;
 });
 
+//Play Button
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
 	video.play();
 });
+
+//Pause Button
+document.querySelector("#pause").addEventListener("click", function(){
+	console.log("Pause Video");
+	video.pause();
+	document.querySelector("#volume").textContent = Math.round(video.volume * 100) + "%";
+});
+
+//Slow Down
+document.querySelector("#slower").addEventListener("click", function() {
+	console.log(video.playbackRate *= 0.90);
+	
+});
+
+//Speed Up
+document.querySelector("#faster").addEventListener("click", function() {
+	console.log(video.playbackRate /= 0.90);
+	
+});
+
+//Skip Ahead
+document.querySelector("#skip").addEventListener("click", function() {
+	if (video.currentTime + 10 > video.duration) {
+		console.log(video.currentTime = 0);
+	} else {
+		console.log(video.currentTime += 10);
+	}
+
+	
+});
+//Mute
+
+//Volume Slider
+
+//Styled
+
+//Original
 
 
